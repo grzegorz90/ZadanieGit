60d17396ea775d723f034d5e875d5904e28c1e37
grzegorz90 <ggelskik@gmail.com>
2014-05-25 21:49:00 +0200

    Dodanie roznicy

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,11 @@
 
-def suma(a, b):
-    return a + b
+def suma(liczba1, liczba2):
+    return liczba1 + liczba2
+
+def roznica(liczba1, liczba2):
+    return liczba1 - liczba2
 
 liczba1 = int(input())
 liczba2 = int(input())
 print("Suma: " + str(suma(liczba1,liczba2)))
+print("Roznica: " + str(roznica(liczba1,liczba2)))
